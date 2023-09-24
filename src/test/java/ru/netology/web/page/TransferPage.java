178c536a53a61fcfc1ea333570d0bc04d89415e7
Alex.Sn <alex7777@nxt.ru>
2023-09-24 12:10:49 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/page/TransferPage.java
@@ -0,0 +1,41 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+import ru.netology.web.data.DataHelper;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+    private SelenideElement heading = $(byText("Пополнение карты"));
+    private SelenideElement amount = $("[data-test-id= 'amount'] input");
+    private SelenideElement from = $("[data-test-id= 'from'] input");
+    private SelenideElement to = $("[data-test-id= 'to'] input");
+    private SelenideElement transferButton = $("[data-test-id= 'action-transfer']");
+    private SelenideElement cancelButton = $("[data-test-id= 'action-cancel']");
+    private SelenideElement errorNotification = $("[data-test-id= 'error-notification']");
+
+    public TransferPage() {
+        heading.shouldBe(visible);
+    }
+
+    public DashboardPage makeValidTransfer(String amountTransfer, DataHelper.CardInfo cardInfo) {
+        makeTransfer(amountTransfer, cardInfo);
+        return new DashboardPage();
+    }
+
+    public void makeTransfer(String amountTransfer, DataHelper.CardInfo cardInfo) {
+        amount.setValue(amountTransfer);
+        from.setValue(cardInfo.getCardNumber());
+        transferButton.click();
+    }
+
+    public void findErrorMessage(String errorText) {
+        errorNotification.shouldBe(visible).shouldHave(exactText(errorText), Duration.ofSeconds(15));
+    }
+}
